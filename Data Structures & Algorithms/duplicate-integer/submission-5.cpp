72feba5591691bf7fc8e33e2dc90406a19f5d021
Matan Eshel <eshel.matan@gmail.com>
2026-08-26 18:36:24 +0300

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+class Solution {
+    #include <unordered_set>
+    #include <iostream>
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> mySet;
+        for (auto itr = nums.begin(); itr != nums.end(); itr++){
+            if (mySet.count(*itr)) {
+                return true;
+            }
+            mySet.insert(*itr);
+        }
+        return false;
+    }
+};
